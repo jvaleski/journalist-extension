35bbb954d61a22cfaf4bcfbd5b6a40c15b7152a9
Jud Valeski <jvaleski@gmail.com>
2016-12-04 22:39:06 +0000

    minor update

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 // blogger
 //alert(document.getElementsByClassName('post-author vcard')[0].childNodes[1].children[1].href);
 
-// arstechnia
+// byline structure
 alert(document.getElementsByClassName('byline')[0].childNodes[1].href);

9d42bd53889cfbc9d04790cedcfd2143abced912
Jud Valeski <jvaleski@gmail.com>
2016-12-04 22:28:20 +0000

    updading to content script instead of popup

---
+++ content.js
@@ -0,0 +1,5 @@
+// blogger
+//alert(document.getElementsByClassName('post-author vcard')[0].childNodes[1].children[1].href);
+
+// arstechnia
+alert(document.getElementsByClassName('byline')[0].childNodes[1].href);

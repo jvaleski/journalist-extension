7c7a07c11d963c92445f7fe02b022a05b7f5fa02
Jud Valeski <jvaleski@gmail.com>
2016-12-06 22:34:56 +0000

    adding rudimentary messages passing to get info into a popup. KNOWN BUG around losing context if you navigate between tabs and click the button.

--- content.js
+++ content.js
@@ -6,6 +6,11 @@ var authorHREFString = "";
 for (var x in nodePossibilities) {
     var elements = document.getElementsByClassName(nodePossibilities[x]);
     for (var i = 0; i < elements.length; i++) {
+	var author_name = elements[i].getElementsByTagName('span');
+        if (author_name.length > 0) {
+          authorHREFString += author_name[0].innerText + " - ";
+        }
+
         var anchor = elements[i].getElementsByTagName('a');
         if (anchor.length > 0) {
             authorHREFString += anchor[0].href + "\n\n\n";
@@ -16,3 +21,7 @@ for (var x in nodePossibilities) {
 if (authorHREFString.length > 0) {
     alert(authorHREFString);
 }
+
+chrome.runtime.sendMessage({sender: "content_script", data: authorHREFString}, function(response) {
+  //alert(response.farewell);
+});
